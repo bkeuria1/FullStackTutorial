d92ee3b69e20e70bddcb88286952cc50c205e2fa
bkeuria1 <benkeurian@gmail.com>
2021-12-24 01:39:54 -0500

    Working on Show,Edit, Delete

---
+++ index.js
@@ -0,0 +1,1 @@
+<h2>Recently Added</h2>
--- server.js
+++ server.js
@@ -3,11 +3,17 @@ if(process.env.NODE_ENV !== 'production'){
 }
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 const expressLayouts = require('express-ejs-layouts')
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 const indexRouter = require('./routes/index')
+const authorRouter = require ('./routes/authors')
+const bookRouter = require ('./routes/books')
 app.set('view engine', 'ejs')
 app.set('views',__dirname+'/views')
 app.set('layout','layouts/layout')
+app.use(methodOverride('_method'))
 app.use(expressLayouts)
 
 
@@ -18,5 +24,8 @@ db.on('error', error=> console.error(error))
 db.once('open', ()=> console.log('Connected'))
 
 app.use('/',indexRouter)
+app.use('/authors',authorRouter)
+app.use('/books',bookRouter)
+
 app.use(express.static('public'))
 app.listen(process.env.PORT || 3000)
